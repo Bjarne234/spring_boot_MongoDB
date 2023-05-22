70104f25be1f4225e9daecdb57f5ae28208abd0b
Bjarne Jensen <b.bjarnejensen@web.de>
2023-05-22 13:56:42 +0200

    update

---
+++ accessing-data-mongodb/src/test/java/bj/spring/com/example/accessingdatamongodb/AccessingDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package bj.spring.com.example.accessingdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
