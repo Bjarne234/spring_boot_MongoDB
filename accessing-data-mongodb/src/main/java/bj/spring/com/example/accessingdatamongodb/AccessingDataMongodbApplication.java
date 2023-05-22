7aad9075889ff0cf42e6e6f09d37554ef167efcc
Bjarne Jensen <b.bjarnejensen@web.de>
2023-05-22 15:21:32 +0200

    Update CustomerRepository
    Finish first attribute in MongoDB local

--- accessing-data-mongodb/src/main/java/bj/spring/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
+++ accessing-data-mongodb/src/main/java/bj/spring/com/example/accessingdatamongodb/AccessingDataMongodbApplication.java
@@ -1,13 +1,50 @@
 package bj.spring.com.example.accessingdatamongodb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main Klasse als Startpunkt, mit SpringApplication.run wird spring klasse gestartet
+
 @SpringBootApplication
-public class AccessingDataMongodbApplication {
+public class AccessingDataMongodbApplication implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataMongodbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new Customer("Alice1", "Smith1"));
+		repository.save(new Customer("Bob1", "Smith1"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice1'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice1"));
+
+		System.out.println("Customers found with findByLastName('Smith1'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith1")) {
+			System.out.println(customer);
+		}
+
 	}
 
 }
