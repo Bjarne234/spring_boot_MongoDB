0cc0126cc15b0bf264ae64eebb428f3d172095f8
Bjarne Jensen <61106749+Bjarne234@users.noreply.github.com>
2023-05-30 09:55:12 +0200

    up

--- accessing-data-mongodb/src/main/java/bj/spring/com/example/accessingdatamongodb/CustomerController.java
+++ accessing-data-mongodb/src/main/java/bj/spring/com/example/accessingdatamongodb/CustomerController.java
@@ -1,7 +1,10 @@
 package bj.spring.com.example.accessingdatamongodb;
 
 import lombok.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.annotation.Id;
 import java.util.List;
 
@@ -23,6 +26,6 @@ public class CustomerController {
     // post Endpunkt
     @GetMapping("/person")
     public void saveUserByName(@RequestParam(value = "id") long id, @RequestParam(value = "firstName") String firstName) {
-        return savePerson(id, name)
+        return savePerson(id, firstNames);
     }
 }
